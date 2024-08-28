9f4b11632dfdce3666e4c06fbd7711c3854b1633
sebas2426 <tobiaspalma505@gmail.com>
2024-08-28 18:56:32 -0400

    Resolve the 01,02,03 and half of 04 exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,29 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array,num1, ...moreArgs) 
+{
+const completeArray=array;
+if(moreArgs.length===0){
+        let indexCut1=array.indexOf(num1);
+        let removedArray=array.splice(indexCut1,1);
+        for(let i=0;i<array.length;i++){
+            if(num1===array[i]){
+                indexCut1=array.indexOf(num1);
+                removedArray=array.splice(indexCut1,1);
+            }
+        }
+        return array; 
 
+}else if (moreArgs.length!=0){
+    if(num1>completeArray.length){
+        return completeArray;
+    }else{
+    let moreArgsValue=moreArgs[0];
+    let indexCut2=array.indexOf(moreArgsValue);
+    let removedArray2=array.splice(indexCut2,2);
+    return array;
+    }
+}
+}
+
+removeFromArray([1, 2, 3, 4], 7, "tacos"); 
 // Do not edit below this line
 module.exports = removeFromArray;
